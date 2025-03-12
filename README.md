580b80ca86dcb06aae051cbfd1cbfd62d8f2cf6e
menzieking <menzie.king@mgschools.net>
2025-03-12 09:58:55 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rizzpizz
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/menzieking/rizzpizz)
---
+++ script.js
@@ -0,0 +1,12 @@
+// Function to get random screen position
+function getRandomPosition() {
+  const width = window.screen.width - 400;  // Window width
+  const height = window.screen.height - 400; // Window height
+  
+  return `width=400,height=400,left=${Math.floor(Math.random() * width)},top=${Math.floor(Math.random() * height)}`;
+}
+
+// Open new windows every second
+setInterval(() => {
+  window.open('', '_blank', getRandomPosition());
+}, 1000);
